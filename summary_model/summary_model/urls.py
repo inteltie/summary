653c1aa5e1df4fb3e939c449ed292dc2ecb4f4e4
stshubham1820 <st18msd7@gmail.com>
2024-04-11 11:27:55 +0530

    url_updated

--- summary_model/summary_model/urls.py
+++ summary_model/summary_model/urls.py
@@ -19,5 +19,6 @@ from django.urls import path
 from summary.views import *
 
 urlpatterns = [
-    path('api/v1/meetings/<int:meeting_id>/summary/', Summary.as_view(), name='action_items'),
+    path('api/v1/meetings/<int:meeting_id>/summary/', Summary.as_view(), name='summary'),
+    path('api/v1/meetings/<int:meeting_id>/update_summary/', SummaryUpdateView.as_view(), name='summary_update'),
 ]
